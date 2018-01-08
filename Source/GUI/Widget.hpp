2d27a95601a1057069ddc964a3283bac7db9279e
KomorKoo <35236681+KomorKoo@users.noreply.github.com>
2018-01-08 20:56:27 +0100

    Update Widget.hpp

--- Source/GUI/Widget.hpp
+++ Source/GUI/Widget.hpp
@@ -17,9 +17,9 @@ class Widget {
 		};
 
 	public:
-		virtual bool handleEvents	(const sf::RenderWindow& window, sf::Event ev) = 0;
-		virtual void render			(sf::RenderTarget& renderer)				   = 0;
-		virtual void setPosition	(const sf::Vector2f& pos)					   = 0;
-		virtual sf::Vector2f getSize() const									   = 0;
-		virtual sf::Vector2f getPos() const										   = 0;
+		virtual bool handleEvents(const sf::RenderWindow& window, sf::Event ev) = 0;
+		virtual void render(sf::RenderTarget& renderer)				= 0;
+		virtual void setPosition(const sf::Vector2f& pos)			= 0;
+		virtual sf::Vector2f getSize() const					= 0;
+		virtual sf::Vector2f getPos() const					= 0;
 };
