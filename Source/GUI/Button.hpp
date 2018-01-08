7939bbee4fcfdab79f0e52ddf80f2d6636193b6e
KomorKoo <35236681+KomorKoo@users.noreply.github.com>
2018-01-08 20:57:19 +0100

    Update Button.hpp

--- Source/GUI/Button.hpp
+++ Source/GUI/Button.hpp
@@ -16,7 +16,7 @@ class Button : public Widget {
 	private:
 		Rectangle	 m_buttonShape;
 		Text		 m_buttonText;
-		sf::Vector2f m_buttonPos;
+		sf::Vector2f 	 m_buttonPos;
 
 		std::function<void(void)> m_function = []() {};
 
@@ -32,8 +32,8 @@ class Button : public Widget {
 		void setTexture	(const sf::Texture& texture);
 
 		bool handleEvents(const sf::RenderWindow& window, sf::Event ev) override;
-		void render(sf::RenderTarget& renderer)							override;
-		void setPosition(const sf::Vector2f& pos)						override;
-		sf::Vector2f getSize() const									override;
-		sf::Vector2f getPos() const										override;
+		void render(sf::RenderTarget& renderer)				override;
+		void setPosition(const sf::Vector2f& pos)			override;
+		sf::Vector2f getSize() const					override;
+		sf::Vector2f getPos() const					override;
 };
